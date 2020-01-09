89d425682d2d681b4452da9e9be89d5eb96bf980
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-01-08 20:43:13 -0600

    added some routes, set a starting script

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,7 @@
 //Dependencies
 var express = require("express");
 var path = require("path");
-var fs = requise("fs");
+var fs = require("fs");
 
 //Setting up express app
 var app = express();
@@ -10,6 +10,30 @@ var PORT = process.env.PORT || 3000;
 //Sets up Express so that it can parse data
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+//Routes
+
+//route path for the homepage
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+//route path for the notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+//look at this some more
+app.get("/api/notes", function(req, res) {
+    var dir = fs.readdir("/develop/db/db.json");
+    return dir;
+})
+
+//
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    console.log(newNote)
+})
 
 //Listener
 app.listen(PORT, function() {
